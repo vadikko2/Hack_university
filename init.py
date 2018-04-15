6aa471d63b24dbc6e76b5536e89a55458e9d185e
wkpn <yegornosov@gmail.com>
2018-04-15 11:30:35 +0300

    data

--- init.py
+++ init.py
@@ -20,7 +20,7 @@ def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
     cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
     cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness)
 
-source = 'data/test.mp4'
+source = 'data/testing.mp4'
 
 db = database.DataBase('inarea.json')
 cap = cv2.VideoCapture(source)
@@ -102,7 +102,32 @@ db.getstatistic(t, 7)
 cv2.destroyAllWindows()
 cap.release()
 
-import webbrowser
-import web
-web.main()
-webbrowser.open_new('localhost:8082')
+from aiohttp import web
+from handlers.main import main_body
+import json
+import asyncio
+
+
+def main():
+    app = web.Application()
+
+    app.router.add_static(prefix='/js/', path='./js/')
+    app.router.add_static(prefix='/css/', path='./css/')
+
+    male_data = json.load(open('male.json', 'r'))
+    female_data = json.load(open('female.json', 'r'))
+
+    async def main_handler(data):
+        return web.Response(body=main_body(male_data=male_data, female_data=female_data), content_type='text/html')
+
+    app.router.add_route('GET', '/', main_handler)
+
+    loop = asyncio.get_event_loop()
+    handler = app.make_handler()
+
+    server = loop.create_server(handler, 'localhost', 8082)
+
+    loop.run_until_complete(server)
+    loop.run_forever()
+
+main()
